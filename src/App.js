77097e4035ff454f5b28941cd68ecdfb6de68c95
manojreactjs <32819260+manojreactjs@users.noreply.github.com>
2017-12-12 15:56:25 -0600

    Changes made

--- src/App.js
+++ src/App.js
@@ -13,13 +13,7 @@ class App extends Component {
        <h2>Welcome to maths game</h2>
        <h3>Hey this game is very interesting</h3>
        <h3>This is very good requirement</h3>
-       <h5>Nice to learn this subject even after</h5>
-       <h2>Welcome to maths game</h2>
-      <h3>Hey this game is very interesting</h3>
-      <h3>This is very good requirement</h3>
-      <p>I Am adding this tag as admin</p>
-      <h5>Nice to learn this subject even after</h5>
-      <h3>Requirement changed</h3>
+       <p>Working on command line</p>
       <h5>Hey this is manoj</h5>
       <h1>you got some explosure on it<h1>
       <h2>Welcome to this great world nothing to make changes</h2>
